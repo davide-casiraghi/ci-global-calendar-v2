33cf25eee68269296a14a7fa67168c368e6b188f
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-12-01 12:28:11 +0100

    Create Manager menu item

--- resources/views/partials/navigation/navigation.blade.php
+++ resources/views/partials/navigation/navigation.blade.php
@@ -19,11 +19,14 @@
             </div>
 
             <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-                <a href="#" class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
-                    Sign in
+                <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">
+                    Manager
+                </a>
+                <a href="#" class="ml-8 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+                    Login
                 </a>
                 <a href="#" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
-                    Sign up
+                    Create account
                 </a>
             </div>
         </div>
