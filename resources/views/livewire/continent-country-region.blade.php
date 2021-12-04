121fdbef10f933d90b545eb9694f8eb837306cf7
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-12-04 11:51:33 +0100

    Hide labels Continent, Country, Region from livewire component.

--- resources/views/livewire/continent-country-region.blade.php
+++ resources/views/livewire/continent-country-region.blade.php
@@ -1,12 +1,17 @@
+@php
+    $selectTailwindClasses = "mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm";
+    $optionTailwindClasses = "text-gray-500";
+@endphp
+
 <div>
     <div class="form-group row">
-        <label for="continent" class="col-md-4 col-form-label text-md-right">Continent</label>
+       {{--<label for="continent" class="col-md-4 col-form-label text-md-right">Continent</label>--}}
 
         <div class="col-md-6">
-            <select wire:model="selectedContinent" class="form-control">
+            <select wire:model="selectedContinent" class="form-control {{$selectTailwindClasses}}">
                 <option value="" selected>Choose continent</option>
                 @foreach($continents as $continent)
-                    <option value="{{ $continent->id }}">{{ $continent->name }}</option>
+                    <option value="{{ $continent->id }}" class="{{$optionTailwindClasses}}">{{ $continent->name }}</option>
                 @endforeach
             </select>
         </div>
@@ -14,13 +19,13 @@
 
     @if (!is_null($selectedContinent))
         <div class="form-group row">
-            <label for="country" class="col-md-4 col-form-label text-md-right">Country</label>
+            {{--<label for="country" class="col-md-4 col-form-label text-md-right">Country</label>--}}
 
             <div class="col-md-6">
-                <select wire:model="selectedCountry" class="form-control">
+                <select wire:model="selectedCountry" class="form-control {{$selectTailwindClasses}}">
                     <option value="" selected>Choose country</option>
                     @foreach($countries as $country)
-                        <option value="{{ $country->id }}">{{ $country->name }}</option>
+                        <option value="{{ $country->id }}" class="{{$optionTailwindClasses}}">{{ $country->name }}</option>
                     @endforeach
                 </select>
             </div>
@@ -29,13 +34,13 @@
 
     @if (!is_null($selectedCountry))
         <div class="form-group row">
-            <label for="region" class="col-md-4 col-form-label text-md-right">Region</label>
+            {{--<label for="region" class="col-md-4 col-form-label text-md-right">Region</label>--}}
 
             <div class="col-md-6">
-                <select wire:model="selectedRegion" class="form-control" name="region_id">
+                <select wire:model="selectedRegion" class="form-control {{$selectTailwindClasses}}" name="region_id">
                     <option value="" selected>Choose region</option>
                     @foreach($regions as $region)
-                        <option value="{{ $region->id }}">{{ $region->name }}</option>
+                        <option value="{{ $region->id }}" class="{{$optionTailwindClasses}}">{{ $region->name }}</option>
                     @endforeach
                 </select>
             </div>
