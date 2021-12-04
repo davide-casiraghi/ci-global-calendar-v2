ce03dcce43599040b56c8019a50d821e38a384f3
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-12-04 12:42:54 +0100

    Add spacing between HP filters.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,11 +39,10 @@
         </p>
     </div>
 
-    <div>
+    <div class="mb-10">
         <div class="md:grid md:grid-cols-6 md:gap-4 max-w-4xl m-auto">
             <div class="md:col-span-2">
                 <b>What</b>
-
                 @include('partials.forms.select', [
                                 'label' => "",
                                 'name' => 'event_category_id',
@@ -54,18 +53,18 @@
                                 'extraClasses' => '',
                             ])
 
-                <b>Who</b>
-
-                @include('partials.forms.select', [
-                                'label' => "",
-                                'name' => 'teacher_id',
-                                'placeholder' => __('views.select_teacher'),
-                                'records' => $teachers,
-                                'selected' => old('teacher_id'),
-                                'required' => TRUE,
-                                'extraClasses' => '',
-                            ])
-
+                <div class="mt-4">
+                    <b>Who</b>
+                    @include('partials.forms.select', [
+                                    'label' => "",
+                                    'name' => 'teacher_id',
+                                    'placeholder' => __('views.select_teacher'),
+                                    'records' => $teachers,
+                                    'selected' => old('teacher_id'),
+                                    'required' => TRUE,
+                                    'extraClasses' => '',
+                                ])
+                </div>
             </div>
             <div class="md:col-span-2 mt-5 md:mt-0">
                 <b>Where</b>
@@ -85,16 +84,17 @@
                                 'disabled' => false,
                             ])
 
-
-                @include('partials.forms.inputDatePicker',[
-                                    'class' => 'flatpickr date future',
-                                    'label' => "",
-                                    'placeholder' => __('views.select_date'),
-                                    'name' => 'endDate',
-                                    'value' => old('endDate'),
-                                    'required' => true,
-                                    'disabled' => false,
-                                ])
+                <div class="mt-4">
+                    @include('partials.forms.inputDatePicker',[
+                                        'class' => 'flatpickr date future',
+                                        'label' => "",
+                                        'placeholder' => __('views.select_date'),
+                                        'name' => 'endDate',
+                                        'value' => old('endDate'),
+                                        'required' => true,
+                                        'disabled' => false,
+                                    ])
+                </div>
             </div>
         </div>
     </div>
