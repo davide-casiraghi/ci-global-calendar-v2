2b715a63c5d6dfb6489003344cd50dba61adfb78
Davide Casiraghi <davide.casiraghi@gmail.com>
2021-12-19 15:11:56 +0100

    Fix eventSearch responsive layout.

--- resources/views/partials/events/searchBar.blade.php
+++ resources/views/partials/events/searchBar.blade.php
@@ -1,8 +1,8 @@
 {{-- Search bar - Posts --}}
 <form id="searchPostsForm" method="get" action="#" class="mb-4">
-    <div class="md:grid md:grid-cols-6 md:gap-2">
+    <div class="md:grid md:grid-cols-12 md:gap-2">
         {{-- Title --}}
-        <div class="md:col-span-2 lg:col-span-1 mb-2 md:mb-0">
+        <div class="md:col-span-4 xl:col-span-2 mb-2 md:mb-0">
             @include('partials.forms.input', [
                             'label' => __('views.title'),
                             'name' => 'title',
@@ -14,7 +14,7 @@
         </div>
 
         {{-- Category --}}
-        <div class="md:col-span-2 lg:col-span-1 mb-2 md:mb-0">
+        <div class="md:col-span-4 xl:col-span-2 mb-2 md:mb-0">
             @include('partials.forms.select', [
                         'label' => __('views.category'),
                         'name' => 'eventCategoryId',
@@ -26,8 +26,20 @@
                     ])
         </div>
 
+        {{-- Status --}}
+        <div class="md:col-span-4 xl:col-span-2 mb-2 md:mb-0">
+            @include('partials.forms.select_status', [
+                       'label' => __('views.status'),
+                       'name' => 'is_published',
+                       'placeholder' => __('views.select_one'),
+                       'records' => $statuses,
+                       'selected' =>  old('is_published', $searchParameters['is_published']),
+                       'required' => false,
+                   ])
+        </div>
+
         {{-- Creation date before --}}
-        <div class="md:col-span-2 lg:col-span-1 mb-2 md:mb-0">
+        <div class="md:col-span-4 xl:col-span-2 mb-2 md:mb-0">
             @include('partials.forms.inputDatePicker',[
                 'class' => 'datepicker all',
                 'label' => __('event.date_start'),
@@ -40,7 +52,7 @@
         </div>
 
         {{-- Creation date after --}}
-        <div class="md:col-span-2 lg:col-span-1 mb-2 md:mb-0">
+        <div class="md:col-span-4 xl:col-span-2 mb-2 md:mb-0">
             @include('partials.forms.inputDatePicker',[
                 'class' => 'datepicker all',
                 'label' => __('event.date_end'),
@@ -52,20 +64,8 @@
             ])
         </div>
 
-        {{-- Status --}}
-        <div class="md:col-span-2 lg:col-span-1 mb-2 md:mb-0">
-            @include('partials.forms.select_status', [
-                       'label' => __('views.status'),
-                       'name' => 'is_published',
-                       'placeholder' => __('views.select_one'),
-                       'records' => $statuses,
-                       'selected' =>  old('is_published', $searchParameters['is_published']),
-                       'required' => false,
-                   ])
-        </div>
-
         {{-- Search / Reset buttons --}}
-        <div class="md:col-span-2 lg:col-span-1 flex items-end justify-end mt-4 md:mt-0 mb-2">
+        <div class="md:col-span-4 xl:col-span-2 flex items-end justify-end mt-4 md:mt-0 mb-2">
 
             @include('partials.forms.button_submit',[
                      'title' => __('general.search'),
