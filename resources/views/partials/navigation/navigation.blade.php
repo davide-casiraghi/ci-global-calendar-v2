3248ab679b107f5273c77d9dccb56a194ec69d38
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-01-07 14:27:31 +0100

    Fix login link in Navbar.

--- resources/views/partials/navigation/navigation.blade.php
+++ resources/views/partials/navigation/navigation.blade.php
@@ -91,7 +91,7 @@
                         </div>
                     </div>
                 </div>
-                <a href="#" class="ml-8 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+                <a href="{{route('login')}}" class="ml-8 whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
                     Login
                 </a>
                 <a href="#" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
