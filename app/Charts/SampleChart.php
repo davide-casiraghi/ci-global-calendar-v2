774e108d36212962121409ed191c1c8106a7f4c3
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-01-08 10:47:07 +0100

    Create UsersOrganizersTeachersEvents Chart.

--- app/Charts/SampleChart.php
+++ app/Charts/SampleChart.php
@@ -4,6 +4,8 @@ declare(strict_types = 1);
 
 namespace App\Charts;
 
+use App\Models\Statistic;
+use Carbon\Carbon;
 use Chartisan\PHP\Chartisan;
 use ConsoleTVs\Charts\BaseChart;
 use Illuminate\Http\Request;
@@ -17,9 +19,35 @@ class SampleChart extends BaseChart
      */
     public function handler(Request $request): Chartisan
     {
-        return Chartisan::build()
-            ->labels(['First', 'Second', 'Third'])
-            ->dataset('Sample', [1, 2, 3])
-            ->dataset('Sample 2', [3, 2, 1]);
+
+        $labels = [];
+        $dataRegisteredUsers = [];
+        $dataOrganizerProfiles = [];
+        $dataTeacherProfiles = [];
+        $dataActiveEvents = [];
+
+        $daysRange = 5;
+        $lastIDUpdatedStats = Statistic::max('id');
+
+
+        for ($days_backwards = $daysRange; $days_backwards >= 0; $days_backwards--) {
+            $dayStat = Statistic::find($lastIDUpdatedStats - $days_backwards);
+            $dataRegisteredUsers[] = $dayStat->registered_users_number;
+            $dataOrganizerProfiles[] = $dayStat->organizers_number;
+            $dataTeacherProfiles[] = $dayStat->teachers_number;
+            $dataActiveEvents[] = $dayStat->active_events_number;
+            $labels[] = Carbon::parse($dayStat->created_at)->format('d/m');
+        }
+        
+
+        $ret = Chartisan::build();
+        $ret->labels($labels);
+
+        $ret->dataset('Registered Users', $dataRegisteredUsers);
+        $ret->dataset('Organizer profiles', $dataOrganizerProfiles);
+        $ret->dataset('Teacher profiles', $dataTeacherProfiles);
+        $ret->dataset('Active events', $dataActiveEvents);
+
+        return $ret;
     }
 }
