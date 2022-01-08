d7e120963146f2d08c410ab19cf048d3504da455
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-01-08 13:30:09 +0100

    Create TeacherByCountryGraph.

--- app/Charts/TeachersByCountryChart.php
+++ app/Charts/TeachersByCountryChart.php
@@ -4,11 +4,11 @@ declare(strict_types = 1);
 
 namespace App\Charts;
 
-use App\Models\Statistic;
-use Carbon\Carbon;
+use App\Models\Teacher;
 use Chartisan\PHP\Chartisan;
 use ConsoleTVs\Charts\BaseChart;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TeachersByCountryChart extends BaseChart
 {
@@ -19,31 +19,23 @@ class TeachersByCountryChart extends BaseChart
      */
     public function handler(Request $request): Chartisan
     {
+        $teachersNumberByCountries = Teacher::leftJoin('countries', 'teachers.country_id', '=', 'countries.id')
+            ->select(DB::raw('count(*) as teacher_count, countries.name as country_name'))
+            ->groupBy('country_id')
+            ->orderBy('country_name')
+            ->get();
+
+        $data = [];
         $labels = [];
-        $dataRegisteredUsers = [];
-        $dataOrganizerProfiles = [];
-        $dataTeacherProfiles = [];
-        $dataActiveEvents = [];
-
-        $daysRange = 5; // todo - set for a longer period eg.14 days
-        $lastIDUpdatedStats = Statistic::max('id');
-
-        for ($days_backwards = $daysRange; $days_backwards >= 0; $days_backwards--) {
-            $dayStat = Statistic::find($lastIDUpdatedStats - $days_backwards);
-            $dataRegisteredUsers[] = $dayStat->registered_users_number;
-            $dataOrganizerProfiles[] = $dayStat->organizers_number;
-            $dataTeacherProfiles[] = $dayStat->teachers_number;
-            $dataActiveEvents[] = $dayStat->active_events_number;
-            $labels[] = Carbon::parse($dayStat->created_at)->format('d/m');
+        foreach ($teachersNumberByCountries as $key => $teachersNumberByCountry) {
+            $data[] = $teachersNumberByCountry->teacher_count;
+            $labels[] = $teachersNumberByCountry->country_name;
         }
 
         $ret = Chartisan::build();
         $ret->labels($labels);
 
-        $ret->dataset('Registered Users', $dataRegisteredUsers);
-        $ret->dataset('Organizer profiles', $dataOrganizerProfiles);
-        $ret->dataset('Teacher profiles', $dataTeacherProfiles);
-        $ret->dataset('Active events', $dataActiveEvents);
+        $ret->dataset('Teachers by country', $data);
 
         return $ret;
     }
--- resources/views/statistics/index.blade.php
+++ resources/views/statistics/index.blade.php
@@ -8,7 +8,7 @@
     <script src="https://unpkg.com/@chartisan/echarts/dist/chartisan_echarts.js"></script>
     <!-- Your application script -->
     <script>
-        const chart = new Chartisan({
+        const chart1 = new Chartisan({
             el: '#chartSummaryChart',
             url: "@chart('summary_chart')",
             hooks: new ChartisanHooks()
@@ -17,11 +17,18 @@
                 //.datasets([{ type: 'line', fill: false }, 'bar']),
         });
 
-
-
-        const chart1 = new Chartisan({
+        const chart2 = new Chartisan({
             el: '#chartUsersByCountry',
             url: "@chart('users_by_country_chart')",
+            hooks: new ChartisanHooks()
+                .colors()
+                .datasets([{ type: 'bar', fill: false }]),
+            //.datasets([{ type: 'line', fill: false }, 'bar']),
+        });
+
+        const chart3 = new Chartisan({
+            el: '#chartTeachersByCountry',
+            url: "@chart('teachers_by_country_chart')",
             hooks: new ChartisanHooks()
                 .colors()
                 .datasets([{ type: 'bar', fill: false }]),
@@ -35,11 +42,13 @@
 @endsection
 
 @section('content')
-    
+
     <!-- Summart chart container -->
     <div id="chartSummaryChart" style="height: 300px;"></div>
 
-
     <!-- Users by country container -->
     <div id="chartUsersByCountry" style="height: 300px;"></div>
+
+    <!-- Teachers by country container -->
+    <div id="chartTeachersByCountry" style="height: 300px;"></div>
 @endsection
