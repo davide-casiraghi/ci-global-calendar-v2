9e2f13a137ac4a62870b0090da4d796b40b7465a
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-01-09 09:10:20 +0100

    Customize Graphs adding options.

--- resources/views/statistics/index.blade.php
+++ resources/views/statistics/index.blade.php
@@ -13,7 +13,18 @@
             url: "@chart('summary_chart')",
             hooks: new ChartisanHooks()
                 .colors()
-                .datasets([{ type: 'line', fill: false }]),
+                .datasets([{ type: 'line', fill: false }])
+                .title('Summary chart')
+                .legend({ position: 'bottom' })
+                .tooltip({
+                    enabled: true,
+                    mode: 'single',
+                    callbacks: {
+                        label: function(tooltipItems, data) {
+                            return '$' + addCommas(tooltipItems.yLabel) + '.00';
+                        }
+                    }
+                }),
                 //.datasets([{ type: 'line', fill: false }, 'bar']),
         });
 
@@ -22,16 +33,54 @@
             url: "@chart('users_by_country_chart')",
             hooks: new ChartisanHooks()
                 .colors()
-                .datasets([{ type: 'bar', fill: false }]),
+                .datasets([{ type: 'bar', fill: false }])
+                .title('Users by country'),
             //.datasets([{ type: 'line', fill: false }, 'bar']),
         });
 
+        //todo - check this good example for documentation.
+        //https://github.com/Chartisan/Chartisan/issues/7#issuecomment-774745067
         const chart3 = new Chartisan({
             el: '#chartTeachersByCountry',
             url: "@chart('teachers_by_country_chart')",
             hooks: new ChartisanHooks()
                 .colors()
-                .datasets([{ type: 'bar', fill: false }]),
+                .datasets([{ type: 'bar', fill: false }])
+                .title('Teachers by country')
+                .tooltip({
+                    enabled: true,
+                    mode: 'single',
+                    callbacks: {
+                        label: function(tooltipItems, data) {
+                            return '$' + addCommas(tooltipItems.yLabel) + '.00';
+                        }
+                    }
+                })
+                .custom(function({ data, merge, server }) {
+                    // data ->   Contains the current chart configuration
+                    //           data that will be passed to the chart instance.
+                    // merge ->  Contains a function that can be called to merge
+                    //           two javascript objects and returns its merge.
+                    // server -> Contains the server information in case you need
+                    //           to acces the raw information provided by the server.
+                    //           This is mostly used to access the `extra` field.
+
+                    return merge(data, {
+                        options: {
+                            scales: {
+                                yAxes: [{
+                                    ticks: {
+                                        autoSkip: false,
+                                        maxRotation: 90,
+                                        minRotation: 90
+                                    }
+                                }]
+                            }
+                        }
+                    });
+
+                    // The function must always return the new chart configuration.
+                }),
             //.datasets([{ type: 'line', fill: false }, 'bar']),
         });
     </script>
