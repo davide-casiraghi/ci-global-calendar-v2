696b240175223eeba0abda93d228f678049ef8b3
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-01-11 14:32:43 +0100

    Add manager menu items links.

--- resources/views/partials/navigation/items/firstLevelDropdown.blade.php
+++ resources/views/partials/navigation/items/firstLevelDropdown.blade.php
@@ -13,7 +13,7 @@
 
     <div x-show="open" x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0 translate-y-1" x-transition:enter-end="opacity-100 translate-y-0" x-transition:leave="transition ease-in duration-150" x-transition:leave-start="opacity-100 translate-y-0" x-transition:leave-end="opacity-0 translate-y-1" x-description="'Solutions' flyout menu, show/hide based on flyout menu state." class="absolute z-10 -ml-4 top-14 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0" x-ref="panel" @click.away="open = false">
         <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-            <div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+            <div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-4 sm:p-8">
                 @include($submenu)
             </div>
         </div>
