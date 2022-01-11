db45adcb288d157cd4bb45eb414deec06c3959fe
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-01-11 10:43:08 +0100

    Remove margin between buttons.

--- resources/views/partials/navigation/items/firstLevelBasic.blade.php
+++ resources/views/partials/navigation/items/firstLevelBasic.blade.php
@@ -1,4 +1,4 @@
-<a href="{{$route}}" class="flex px-3 items-center hover:bg-calendarGoldHover ml-3">
+<a href="{{$route}}" class="flex px-3 items-center hover:bg-calendarGoldHover">
     {!! $svg !!}
     <div class="ml-2 text-base font-medium text-white">
         {{$label}}
