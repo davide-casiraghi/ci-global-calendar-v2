10b3e3ab86968bd561a52b8ec7c95285fa53e01a
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-01-13 15:46:05 +0100

    Fix tests localization problem.

--- routes/web.php
+++ routes/web.php
@@ -191,7 +191,7 @@ Route::group(
     //Route::post('/contact', [ContactMeController::class, 'store'])->name('contact.store')->middleware(ProtectAgainstSpam::class);
 
     Route::get('/', [ HomeController::class, 'index'])->name('home');
-    Route::get('/blog', [PostController::class, 'blog'])->name('posts.blog');
+    //Route::get('/blog', [PostController::class, 'blog'])->name('posts.blog');
     Route::get('/posts/{post:slug}', [PostController::class, 'show'])->name('posts.show');
     Route::get('/next-events', [EventController::class, 'nextEvents'])->name('events.next');
     Route::get('/past-events', [EventController::class, 'pastEvents'])->name('events.past');
