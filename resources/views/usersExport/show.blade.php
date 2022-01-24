bcd277daeaa3ac1950245f3941c9f6dd8550dbab
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-01-24 11:22:16 +0100

    Refactor user export button.

--- resources/views/usersExport/show.blade.php
+++ resources/views/usersExport/show.blade.php
@@ -10,11 +10,8 @@
 
     <form action="{{ route('users-export-export') }}" method="POST">
         @csrf
-        <div class="mt-8">
-            <button type="submit"
-                    class="blueButton mediumButton">
-                Export
-            </button>
-        </div>
+        <button type="submit" class="blueButton mediumButton mt-8">
+            Export
+        </button>
     </form>
 @endsection
