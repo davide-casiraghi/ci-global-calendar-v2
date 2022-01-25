e5975d74c8f0f22b3044de17e3922a1d4192012b
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-01-25 09:47:44 +0100

    Fix notification tests.

--- tests/Feature/Services/NotificationServiceTest.php
+++ tests/Feature/Services/NotificationServiceTest.php
@@ -3,17 +3,13 @@
 namespace Tests\Feature\Services;
 
 use App\Http\Requests\CommentStoreRequest;
-use App\Models\Comment;
 use App\Models\Event;
-use App\Models\Post;
-use App\Models\PostCategory;
 use App\Models\User;
 use App\Models\Venue;
-use App\Notifications\ContactMeMailNotification;
 use App\Notifications\ExpiringEventMailNotification;
-use App\Notifications\GetATreatmentMailNotification;
-use App\Notifications\NewTestimonialMailNotification;
-use App\Services\CommentService;
+use App\Notifications\FeedbackMailNotification;
+use App\Notifications\ReportMisuseMailNotification;
+use App\Notifications\WriteForMoreInfoMailNotification;
 use App\Services\NotificationService;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -47,85 +43,13 @@ class NotificationServiceTest extends TestCase
         $this->notificationService = $this->app->make('App\Services\NotificationService');
 
         $this->user1 = User::factory()->create([
-           'email' => 'admin@gmail.com',
+           'email' => 'admin@ciglobalcalendar.net',
         ]);
 
         $this->venues = Venue::factory()->count(3)->create();
         $this->event1 = Event::factory()->create([
             'is_published' => 1
         ]);
-    }
-
-    /** @test */
-    public function itShouldSendContactMeEmailNotification()
-    {
-        Notification::fake();
-
-        // Assert that no notifications were sent
-        Notification::assertNothingSent();
-
-        $data = [
-            'name' => 'Billy',
-            'surname' => 'Red',
-            'email' => 'billy.red@test.com',
-            'message' => 'test message',
-        ];
-
-        $sent = $this->notificationService->sendEmailContactMe($data, $this->user1->id);
-
-        Notification::assertSentTo([$this->user1], ContactMeMailNotification::class);
-        $this->assertEquals(true, $sent);
-    }
-
-    /** @test  */
-    public function itShouldSendGetATreatmentEmailNotification()
-    {
-        Notification::fake();
-
-        // Assert that no notifications were sent
-        Notification::assertNothingSent();
-
-        $data = [
-            'name' => 'Billy',
-            'surname' => 'Red',
-            'email' => 'billy.red@test.com',
-            'phone' => '5550123456789',
-            'mainComplaint' => 'Elit voluptatem Co',
-            'mainComplaintIntensity' => '2',
-            'secondaryComplaint' => 'Voluptatibus dolorum',
-            'secondaryComplaintIntensity' => '5',
-            'personalDataAgreement' => 'on',
-            'contactChoice' => 'sms_whatsapp'
-        ];
-
-        $sent = $this->notificationService->sendEmailGetATreatment($data, $this->user1->id);
-
-        Notification::assertSentTo([$this->user1], GetATreatmentMailNotification::class);
-        $this->assertEquals(true, $sent);
-    }
-
-    /** @test  */
-    public function itShouldSendNewTestimonialEmailNotification()
-    {
-        Notification::fake();
-
-        // Assert that no notifications were sent
-        Notification::assertNothingSent();
-
-        $data = [
-            'feedback' => 'Et quisquam hic et sunt',
-            'name' => 'Driscoll',
-            'surname' => 'Lloyd',
-            'profession' => 'Rerum maxime eum min',
-            'country_id' => '224',
-            'personal_data_agreement' => 'on',
-            'publish_agreement' => 'on'
-        ];
-
-        $sent = $this->notificationService->sendEmailNewTestimonial($data, $this->user1->id);
-
-        Notification::assertSentTo([$this->user1], NewTestimonialMailNotification::class);
-        $this->assertEquals(true, $sent);
     }
 
     /** @test  */
@@ -144,4 +68,58 @@ class NotificationServiceTest extends TestCase
         Notification::assertSentTo([$this->event1->user], ExpiringEventMailNotification::class);
         $this->assertEquals(true, $sent);
     }
+
+    /** @test  */
+    public function itShouldSendFeedbackEmailNotification()
+    {
+        Notification::fake();
+
+        // Assert that no notifications were sent
+        Notification::assertNothingSent();
+
+        $data = [];
+        $data['name'] = 'Robert Red';
+        $data['email'] = 'testemail@test.com';
+        $data['message'] = 'Lorem ipsum message';
+
+        $sent = $this->notificationService->sendEmailFeedback($data);
+        Notification::assertSentTo($this->user1, FeedbackMailNotification::class);
+        $this->assertEquals(true, $sent);
+    }
+
+    /** @test  */
+    public function itShouldSendReportMisuseEmailNotification()
+    {
+        Notification::fake();
+
+        // Assert that no notifications were sent
+        Notification::assertNothingSent();
+
+        $data = [];
+        $data['reason'] = __('misuse.not_translated_english');
+        $data['message'] = 'Please I don\'t understand what the event is about';
+
+        $sent = $this->notificationService->sendEmailReportMisuse($data, $this->event1);
+        Notification::assertSentTo([$this->event1->user], ReportMisuseMailNotification::class);
+        $this->assertEquals(true, $sent);
+    }
+
+    /** @test  */
+    public function itShouldSendWriteForMoreInfoEmailNotification()
+    {
+        Notification::fake();
+
+        // Assert that no notifications were sent
+        Notification::assertNothingSent();
+
+        $data = [];
+        $data['name'] = 'Robert Red';
+        $data['email'] = 'testemail@test.com';
+        $data['message'] = 'Can I have more info please?';
+
+        $sent = $this->notificationService->sendEmailWriteForMoreInfo($data, $this->event1);
+        Notification::assertSentTo([$this->event1->user], WriteForMoreInfoMailNotification::class);
+        $this->assertEquals(true, $sent);
+    }
+
 }
