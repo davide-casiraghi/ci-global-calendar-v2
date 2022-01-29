a6cf498b9b52cb7435ba03d2120ad5670f645d6d
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-01-29 16:45:35 +0100

    Refactor keys in getDonationKindMenuData

--- app/Services/DonationOfferService.php
+++ app/Services/DonationOfferService.php
@@ -91,7 +91,7 @@ class DonationOfferService
     public function getDonationKindMenuData(): array
     {
         return [
-            1 => [
+            'financial' => [
                 'label' => __('donations.donation_kind_financial'),
                 'id' => 'offerFinancial',
                 'icon' => '<svg class="flex-shrink-0 h-6 w-6 text-gray-600 block m-auto" x-description="Heroicon name: outline/cursor-click" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 512 512" stroke="currentColor" aria-hidden="true">
@@ -99,21 +99,21 @@ class DonationOfferService
                             </svg>',
 
             ],
-            2 => [
+            'free_entrance' => [
                 'label' => __('donations.donation_kind_free_entrance'),
                 'id' => 'offerFreeEntrance',
                 'icon' => '<svg class="flex-shrink-0 h-6 w-6 text-gray-600 block m-auto" aria-hidden="true" focusable="false" data-prefix="far" data-icon="ticket-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
                                 <path fill="currentColor" d="M400 208v96H176v-96h224m24-48H152c-13.255 0-24 10.745-24 24v144c0 13.255 10.745 24 24 24h272c13.255 0 24-10.745 24-24V184c0-13.255-10.745-24-24-24zm144 56h8V112c0-26.51-21.49-48-48-48H48C21.49 64 0 85.49 0 112v104h8c22.091 0 40 17.909 40 40s-17.909 40-40 40H0v104c0 26.51 21.49 48 48 48h480c26.51 0 48-21.49 48-48V296h-8c-22.091 0-40-17.909-40-40s17.909-40 40-40zm-40-38.372c-28.47 14.59-48 44.243-48 78.372s19.53 63.782 48 78.372V400H48v-65.628c28.471-14.59 48-44.243 48-78.372s-19.529-63.782-48-78.372V112h480v65.628z" class=""></path>
                             </svg>',
             ],
-            3 => [
+            'volunteer' => [
                 'label' => __('donations.donation_kind_volunteer'),
                 'id' => 'offerVolunteer',
                 'icon' => '<svg class="flex-shrink-0 h-6 w-6 text-gray-600 block m-auto" x-description="Heroicon name: outline/cursor-click" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 512 512" stroke="currentColor" aria-hidden="true">
                                 <path fill="currentColor" d="M637.9 203.9l-8-13.9c-4.4-7.7-14.2-10.3-21.9-5.9l-96.7 56.4c-3.7-27.4-26.9-48.6-55.3-48.6H304v64c0 17.6-14.3 32-32 32s-32-14.4-32-32v-86.3c0-11 5.7-21.3 15-27.1l33.4-20.9c10.2-6.4 21.9-9.7 33.9-9.7h105.3l119.8-68.2c7.7-4.4 10.4-14.1 6-21.8L545.5 8c-4.4-7.7-14.1-10.4-21.8-6L415 64h-92.7c-21 0-41.5 5.9-59.3 17l-33.5 20.9c-18.3 11.4-30.6 29.4-35.2 49.8l-59.4 35.6C110.8 201.8 96 227.9 96 256.1v34.1L8 341c-7.7 4.4-10.3 14.2-5.9 21.9l8 13.9c4.4 7.7 14.2 10.3 21.9 5.9L144 318v-61.8c0-11.3 5.9-21.8 15.6-27.6L192 209v42.2c0 41.8 30 80.1 71.7 84.3 47.9 4.9 88.3-32.7 88.3-79.6v-16h104c4.4 0 8 3.6 8 8v32c0 4.4-3.6 8-8 8h-32v60c0 15.4-12.5 27.8-27.8 27.8h-24.1v24c0 17.8-14.4 32.2-32.2 32.2H211.3l-62.8 36.3c-7.7 4.4-10.3 14.2-5.9 21.9l8 13.9c4.4 7.7 14.2 10.3 21.9 5.9l51.6-29.9h115.8c36.9 0 68.1-25.1 77.4-59.2 31.5-9.2 54.7-38.4 54.7-72.8v-14.3c17.6-5.3 31.5-19 37.1-36.4L632 225.8c7.7-4.5 10.3-14.2 5.9-21.9z" class=""></path>
                             </svg>',
             ],
-            4 => [
+            'other_gift' => [
                 'label' => __('donations.donation_kind_other_gift'),
                 'id' => 'offerOtherGift',
                 'icon' => '<svg class="flex-shrink-0 h-6 w-6 text-gray-600 block m-auto" x-description="Heroicon name: outline/cursor-click" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 512 512" stroke="currentColor" aria-hidden="true">
--- resources/views/partials/donationOffers/offerKindSelector.blade.php
+++ resources/views/partials/donationOffers/offerKindSelector.blade.php
@@ -3,9 +3,9 @@
         <label for="i_can_offer" class="block text-sm font-medium text-gray-700 inline">@lang('donations.i_can_offer')</label>
         <span class="simple-tooltip text-gray-500 inline" title="@lang('views.required')">*</span>
     </div>
-    <div x-data="{ selected: 1 }" class="flex w-full space-x-4 uppercase text-xs font-bold text-gray-500 text-center">
+    <div x-data="{ selected: 'financial' }" class="flex w-full space-x-4 uppercase text-xs font-bold text-gray-500 text-center">
         @foreach($donationKindItems as $key => $donationKindItem)
-            <label x-on:click="selected = {{$key}}" for="{{$donationKindItem['id']}}" class="w-1/4 p-4 rounded border" :class="{ 'border-2 border-green-700 bg-green-100/50': selected === {{$key}} }">
+            <label x-on:click="selected = '{{$key}}'" for="{{$donationKindItem['id']}}" class="w-1/4 p-4 rounded border" :class="{ 'border-2 border-green-700 bg-green-100/50': selected === '{{$key}}' }">
                 <input id="{{$donationKindItem['id']}}" type="radio" name="offer_kind" value="{{$key}}" class="hidden">
                 {!! $donationKindItem['icon'] !!}
                 <div class="block m-auto mt-3">
@@ -14,16 +14,16 @@
             </label>
         @endforeach
 
-            <p x-show="selected === 1">
+            <p x-show="selected == 'financial'">
                 aaaa
             </p>
-            <p x-show="selected === 2">
+            <p x-show="selected == 'free_entrance'">
                 bbbb
             </p>
-            <p x-show="selected === 3">
+            <p x-show="selected == 'volunteer'">
                 cccc
             </p>
-            <p x-show="selected === 4">
+            <p x-show="selected == 'other_gift'">
                 ddd
             </p>
 
