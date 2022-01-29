f9646f8cc0afbadd0dafb184d52cbe72b44a6127
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-01-29 09:07:59 +0100

    Creates I want to help section in donation create form.

--- resources/views/donationOffers/create.blade.php
+++ resources/views/donationOffers/create.blade.php
@@ -10,88 +10,106 @@
 
     <form class="space-y-6" method="POST" action="{{ route('donationOffers.store') }}" enctype="multipart/form-data">
         <div class="bg-white shadow px-4 py-5 sm:rounded-lg sm:p-6">
-          <div class="md:grid md:grid-cols-3 md:gap-6">
-            <div class="md:col-span-1">
-              <h3 class="text-lg font-medium leading-6 text-gray-900">@lang('donations.donation_form_title')</h3>
-                <p class="mt-1 text-sm text-gray-500">
-                    @lang('donations.your_contact_details_desc')
-                </p>
-            </div>
-            <div class="mt-5 md:mt-0 md:col-span-2">
-                @csrf
-                <div class="md:grid md:grid-cols-6 md:gap-x-8 md:gap-y-4">
-                    <div class="md:col-span-3">
-                        @include('partials.forms.input', [
-                                'label' => __('general.name'),
-                                'name' => 'name',
-                                'placeholder' => '',
-                                'value' => old('name'),
+            <div class="md:grid md:grid-cols-3 md:gap-6">
+                <div class="md:col-span-1">
+                    <h3 class="text-lg font-medium leading-6 text-gray-900">@lang('donations.donation_form_title')</h3>
+                    <p class="mt-1 text-sm text-gray-500">
+                        @lang('donations.your_contact_details_desc')
+                    </p>
+                </div>
+                <div class="mt-5 md:mt-0 md:col-span-2">
+                    @csrf
+                    <div class="md:grid md:grid-cols-6 md:gap-x-8 md:gap-y-4">
+                        <div class="md:col-span-3">
+                            @include('partials.forms.input', [
+                                    'label' => __('general.name'),
+                                    'name' => 'name',
+                                    'placeholder' => '',
+                                    'value' => old('name'),
+                                    'required' => true,
+                                    'disabled' => false,
+                            ])
+                        </div>
+
+                        <div class="md:col-span-3 mt-2 md:mt-0">
+                            @include('partials.forms.input', [
+                                    'label' => __('general.surname'),
+                                    'name' => 'surname',
+                                    'placeholder' => '',
+                                    'value' => old('surname'),
+                                    'required' => true,
+                                    'disabled' => false,
+                            ])
+                        </div>
+
+                        <div class="md:col-span-3 mt-2 md:mt-0">
+                            @include('partials.forms.input', [
+                                    'label' => __('general.email_address'),
+                                    'name' => 'email',
+                                    'placeholder' => '',
+                                    'value' => old('email'),
+                                    'required' => true,
+                                    'disabled' => false,
+                            ])
+                        </div>
+
+                        <div class="md:col-span-3 mt-2 md:mt-0">
+                            @include('partials.forms.select', [
+                                'label' => __('general.country'),
+                                'name' => 'country_id',
+                                'placeholder' => __('general.select_one'),
+                                'records' => $countries,
+                                'selected' =>  old('country_id'),
                                 'required' => true,
-                                'disabled' => false,
-                        ])
-                    </div>
+                                'extraClasses' => 'select2',
+                            ])
+                        </div>
 
-                    <div class="md:col-span-3 mt-2 md:mt-0">
-                        @include('partials.forms.input', [
-                                'label' => __('general.surname'),
-                                'name' => 'surname',
-                                'placeholder' => '',
-                                'value' => old('surname'),
-                                'required' => true,
-                                'disabled' => false,
-                        ])
-                    </div>
+                        <div class="md:col-span-3 mt-2 md:mt-0">
+                            @include('partials.forms.input', [
+                                    'label' => __('donations.contact_through_skype_or_another_voip'),
+                                    'name' => 'contact_trough_voip',
+                                    'placeholder' => '',
+                                    'value' => old('contact_trough_voip'),
+                                    'required' => true,
+                                    'disabled' => false,
+                            ])
+                        </div>
 
-                    <div class="md:col-span-3 mt-2 md:mt-0">
-                        @include('partials.forms.input', [
-                                'label' => __('general.email_address'),
-                                'name' => 'email',
-                                'placeholder' => '',
-                                'value' => old('email'),
-                                'required' => true,
-                                'disabled' => false,
-                        ])
-                    </div>
-
-                    <div class="md:col-span-3 mt-2 md:mt-0">
-                        @include('partials.forms.select', [
-                            'label' => __('general.country'),
-                            'name' => 'country_id',
-                            'placeholder' => __('general.select_one'),
-                            'records' => $countries,
-                            'selected' =>  old('country_id'),
-                            'required' => true,
-                            'extraClasses' => 'select2',
-                        ])
-                    </div>
-
-                    <div class="md:col-span-3 mt-2 md:mt-0">
-                        @include('partials.forms.input', [
-                                'label' => __('donations.contact_through_skype_or_another_voip'),
-                                'name' => 'contact_trough_voip',
-                                'placeholder' => '',
-                                'value' => old('contact_trough_voip'),
-                                'required' => true,
-                                'disabled' => false,
-                        ])
-                    </div>
-
-                    <div class="md:col-span-3 mt-2 md:mt-0">
-                        @include('partials.forms.textarea', [
-                               'label' => __('donations.language_spoken'),
-                               'name' => 'language_spoken',
-                               'placeholder' => '',
-                               'value' =>  old('language_spoken'),
-                               'required' => true,
-                               'disabled' => false,
-                               'style' => '',
-                           ])
+                        <div class="md:col-span-3 mt-2 md:mt-0">
+                            @include('partials.forms.textarea', [
+                                   'label' => __('donations.language_spoken'),
+                                   'name' => 'language_spoken',
+                                   'placeholder' => '',
+                                   'value' =>  old('language_spoken'),
+                                   'required' => true,
+                                   'disabled' => false,
+                                   'style' => '',
+                               ])
+                        </div>
                     </div>
                 </div>
-
-                @include('partials.donationOffers.offerKindSelector')
             </div>
-          </div>
+
+            <div class="border-b-2 my-8"></div>
+
+            {{-- I want to help --}}
+            <div class="md:grid md:grid-cols-3 md:gap-6">
+                <div class="md:col-span-1">
+                    <h3 class="text-lg font-medium leading-6 text-gray-900">@lang('donations.i_want_to_help')</h3>
+                    <p class="mt-1 text-sm text-gray-500">
+                        @lang('donations.kind_of_help_description')
+                    </p>
+                </div>
+                <div class="mt-5 md:mt-0 md:col-span-2">
+                    @include('partials.donationOffers.offerKindSelector')
+                </div>
+            </div>
+
+            <div class="border-b-2 my-8"></div>
+
+            
+
         </div>
 
         <div class="flex justify-end">
