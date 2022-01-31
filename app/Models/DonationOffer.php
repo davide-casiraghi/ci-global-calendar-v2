0839f72b4bbda41133ee325f638555f8af8dc207
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-01-31 13:00:18 +0100

    Add constants for GIFT_KIND and VOLUNTEER_KIND to Donations model.

--- app/Models/DonationOffer.php
+++ app/Models/DonationOffer.php
@@ -45,6 +45,27 @@ class DonationOffer extends Model
     ];
 
     /**
+     * The possible values the gift kind can be.
+     * The array items values are language strings.
+     */
+    const GIFT_KIND = [
+        'free_festival' => 'donations.gift_kind_free_festival',
+        'free_other' => 'donations.gift_kind_free_other',
+    ];
+
+    /**
+     * The possible values the gift kind can be.
+     * The array items values are language strings.
+     */
+    const VOLUNTEER_KIND = [
+        'developer' => 'donations.volunteering_kind_developer',
+        'fundraiser' => 'donations.volunteering_kind_fundriser',
+        'translator' => 'donations.volunteering_kind_translator',
+        'communicator' => 'donations.volunteering_kind_communicator',
+        'other' => 'donations.volunteering_kind_other',
+    ];
+
+    /**
      * Return the user that created the organizer
      * @return BelongsTo
      */
