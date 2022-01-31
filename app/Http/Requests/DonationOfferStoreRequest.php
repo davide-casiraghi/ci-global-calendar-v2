69e4d10589d2a614eec3a3d95bfdb37107655014
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-01-31 11:10:07 +0100

    Complete the DonationOfferStoreRequest.

--- app/Http/Requests/DonationOfferStoreRequest.php
+++ app/Http/Requests/DonationOfferStoreRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class DonationOfferStoreRequest extends FormRequest
 {
@@ -29,7 +30,30 @@ class DonationOfferStoreRequest extends FormRequest
             'country_id' => ['required', 'string'],
             'email' => ['required','string','email','max:255'],
             'language_spoken' => ['required', 'string', 'max:255'],
-
+            //'offer_kind' => ['offer_kind', 'string'],
+            'volunteer_kind' => Rule::requiredIf($this->offer_kind == 'volunteer'),
+            'volunteer_description' => Rule::requiredIf($this->offer_kind == 'volunteer'),
+            'gift_title' => Rule::requiredIf(
+                $this->offer_kind == 'free_entrance' ||
+                $this->offer_kind == 'other_gift'
+            ),
+            'gift_donater' => Rule::requiredIf(
+                $this->offer_kind == 'free_entrance' ||
+                $this->offer_kind == 'other_gift'
+            ),
+            'gift_description' => Rule::requiredIf(
+                $this->offer_kind == 'free_entrance' ||
+                $this->offer_kind == 'other_gift'
+            ),
+            'gift_economic_value' => Rule::requiredIf(
+                $this->offer_kind == 'free_entrance' ||
+                $this->offer_kind == 'other_gift'
+            ),
+            'gift_country_of' => Rule::requiredIf(
+                $this->offer_kind == 'free_entrance' ||
+                $this->offer_kind == 'other_gift'
+            ),
+            'gift_kind' => Rule::requiredIf($this->offer_kind == 'free_entrance'),
         ];
     }
 }
