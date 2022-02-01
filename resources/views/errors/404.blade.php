9a639f8c9ce77dfd320c7f8a0310b468dffff6d1
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-02-01 11:36:20 +0100

    Center error message.

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div class="h-screen flex items-center justify-center">
-        <h1 class="text-gray-800 mt-10 text-2xl">
+        <h1 class="text-gray-800 mt-10 text-2xl max-w-lg text-center">
             The page you request has not been found, please report this issue to the <a href="/en/feedback" class="text-blue-600">administrator</a>.
         </h1>
 
