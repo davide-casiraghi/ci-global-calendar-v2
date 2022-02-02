7d1e9776060b067b73b0592a93444376ddad07ad
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-02-02 09:08:48 +0100

    Refactor contextual feedback color.

--- resources/views/partials/contextualFeedback.blade.php
+++ resources/views/partials/contextualFeedback.blade.php
@@ -1,15 +1,5 @@
-@php
-    $backgroundAndTextColors = match ($color) {
-        'warning' => 'bg-yellow-100 text-yellow-700',
-        'success' => 'bg-green-100 text-green-700',
-        'danger' => 'bg-red-100 text-red-700',
-        'gray' => 'bg-gray-100 text-gray-700',
-        default => 'bg-green-100 text-green-700',
-    };
 
-@endphp
-
-<div class="{{$backgroundAndTextColors}} rounded-lg py-5 px-6 text-base {{$extraClasses}}" role="alert">
+<div class="contextualFeedback {{$color}} rounded-lg py-5 px-6 text-base {{$extraClasses}}" role="alert">
     {!! $message !!}
 </div>
 
