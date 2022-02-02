695fa4c5e39e8168c36f16b5ff597a1af272d57d
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-02-02 12:34:35 +0100

    Clean code.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,26 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\PostService;
 use App\Services\StatisticService;
 use Illuminate\Contracts\View\View;
 
 class DashboardController extends Controller
 {
-    private PostService $postService;
     private StatisticService $statisticService;
 
     /**
      * DashboardController constructor.
      *
-     * @param  PostService  $postService
      * @param  StatisticService  $statisticService
      */
     public function __construct(
-        PostService $postService,
         StatisticService $statisticService,
     ) {
-        $this->postService = $postService;
         $this->statisticService = $statisticService;
     }
 
@@ -32,11 +27,9 @@ class DashboardController extends Controller
      */
     public function index(): View
     {
-        $totalPublishedPostsNumber = $this->postService->getPublishedPostsNumber();
         $lastUpdateStatistics = $this->statisticService->getLatestStatistics();
 
         return view('dashboard.index', [
-            'totalPosts' => $totalPublishedPostsNumber,
             'lastUpdateStatistics' => $lastUpdateStatistics
         ]);
     }
