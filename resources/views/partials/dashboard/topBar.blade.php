00665515b3a90936387078cfa8a29e72b20d6eb7
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-02-02 14:56:02 +0100

    Change order of cog submenu items.

--- resources/views/partials/dashboard/topBar.blade.php
+++ resources/views/partials/dashboard/topBar.blade.php
@@ -56,6 +56,25 @@
                      x-transition:leave-end="opacity-0 transform scale-90"
                      @click.away="openSettings = false"
                 >
+
+                    <a href="{{route('homepageMessages.index')}}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 flex" role="menuitem">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
+                        </svg>
+                        <div>
+                            Homepage message
+                        </div>
+                    </a>
+
+                    <a href="{{route('backgroundImages.index')}}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 flex" role="menuitem">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                        </svg>
+                        <div>
+                            Background images
+                        </div>
+                    </a>
+
                     <a href="{{route('teams.index')}}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 flex" role="menuitem">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
@@ -79,24 +98,6 @@
                         </svg>
                         <div>
                             Database backups
-                        </div>
-                    </a>
-
-                    <a href="{{route('backgroundImages.index')}}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 flex" role="menuitem">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                        </svg>
-                        <div>
-                            Background images
-                        </div>
-                    </a>
-
-                    <a href="{{route('homepageMessages.index')}}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 flex" role="menuitem">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
-                        </svg>
-                        <div>
-                            Homepage message
                         </div>
                     </a>
 
