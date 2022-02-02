a2fe42ea24919892ca4561bf6747346e3e04bcb7
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-02-02 14:49:49 +0100

    Move background image menu item to cog submenu.

--- resources/views/partials/dashboard/navigation/desktopMenu.blade.php
+++ resources/views/partials/dashboard/navigation/desktopMenu.blade.php
@@ -83,31 +83,6 @@
                         'label' => 'Donation offers',
                         'url' => route('donationOffers.index'),
                     ])
-
-                    {{--@include('partials.dashboard.navigation.menuItem', [
-                        'active' => request()->routeIs('testimonials*'),
-                        'kind' => 'desktop',
-                        'heroIconPath' => '<path fill-rule="evenodd" d="M18 3a1 1 0 00-1.447-.894L8.763 6H5a3 3 0 000 6h.28l1.771 5.316A1 1 0 008 18h1a1 1 0 001-1v-4.382l6.553 3.276A1 1 0 0018 15V3z" clip-rule="evenodd"></path>',
-                        'label' => 'Testimonials',
-                        'url' => route('testimonials.index'),
-                    ])--}}
-
-                    {{--@include('partials.dashboard.navigation.menuItem', [
-                        'active' => request()->routeIs('insights*'),
-                        'kind' => 'desktop',
-                        'heroIconPath' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"></path>',
-                        'label' => 'Insights',
-                        'url' => route('insights.index'),
-                    ])--}}
-                    
-                    @include('partials.dashboard.navigation.menuItem', [
-                        'active' => request()->routeIs('backgroundImages*'),
-                        'kind' => 'desktop',
-                        'heroIconPath' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />',
-                        'label' => 'Background Images',
-                        'url' => route('backgroundImages.index'),
-                    ])
-                    
                     
                 </nav>
             </div>
--- resources/views/partials/dashboard/navigation/mobileMenu.blade.php
+++ resources/views/partials/dashboard/navigation/mobileMenu.blade.php
@@ -133,14 +133,6 @@
                         'url' => route('homepageMessages.index'),
                     ])
 
-                    @include('partials.dashboard.navigation.menuItem', [
-                        'active' => request()->routeIs('backgroundImages*'),
-                        'kind' => 'mobile',
-                        'heroIconPath' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />',
-                        'label' => 'Background Images',
-                        'url' => route('backgroundImages.index'),
-                    ])
-
                 </nav>
             </div>
         </div>
