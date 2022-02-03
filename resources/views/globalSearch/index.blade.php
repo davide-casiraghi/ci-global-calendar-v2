5dc8745d58554e8d2d00654ee08be1b5a0a2dcbe
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-02-03 10:44:06 +0100

    Create global search index view.

---
+++ resources/views/globalSearch/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.backend')
+
+@section('title')
+    Global search
+@endsection
+
+@section('content')
+
+    <div class="font-bold text-lg">{{ $searchResults->count() }} results found for "{{ request('keyword') }}"</div>
+
+    <div class="my-4">
+
+        @foreach($searchResults->groupByType() as $type => $modelSearchResults)
+            <h2>{{ ucfirst($type) }}</h2>
+
+            @foreach($modelSearchResults as $searchResult)
+                <ul>
+                    <li><a class="text-blue-600" href="{{ $searchResult->url }}">{{ $searchResult->title }}</a></li>
+                </ul>
+            @endforeach
+        @endforeach
+
+    </div>
+
+@endsection
