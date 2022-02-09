b98e504e0cc8e54f1bdd06f80ca5b60d15453f64
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-02-09 14:19:01 +0100

    Center the error message of the 404 page and fix the error message.

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 
-    <div class="h-screen flex items-center justify-center">
+    <div class=" flex items-center justify-center m-auto">
         <h1 class="text-gray-800 mt-10 text-2xl max-w-lg text-center">
-            The page you request has not been found, please report this issue to the <a href="/en/feedback" class="text-blue-600">administrator</a>.
+            The page you request has not been found. <br>
+            You can send a feedback to report the bug using the button located in the bottom right corner.
         </h1>
-
     </div>
 
 @endsection
