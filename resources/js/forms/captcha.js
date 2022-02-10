c93062867dce84c6f77838f053ffbbc47fa86c93
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-02-10 11:17:39 +0100

    Fix captcha not changing error on pages that are not home.

--- resources/js/forms/captcha.js
+++ resources/js/forms/captcha.js
@@ -2,23 +2,24 @@
 $(document).ready(function () {
 
     // Change captcha image.
-    $('#reloadCaptchaImage').on('click',function () {
+    $('.reloadCaptchaImage').on('click',function () {
         $.ajax({
             type: 'GET',
-            url: 'reload-captcha',
+            url: '/reload-captcha',
             success: function (data) {
                 $(".captchaImage").html(data.captcha);
             }
         });
     });
 
-
     // Change captcha image (for Livewire components).
     window.livewire.on('reload_captcha', message => {
+        console.log('click');
         $.ajax({
             type: 'GET',
-            url: 'reload-captcha',
+            url: '/reload-captcha',
             success: function (data) {
+                console.log('success');
                 $(".captchaImage").html(data.captcha);
             }
         });
