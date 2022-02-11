c50725910881924668b01d31408040bdbeafea48
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-02-11 07:20:01 +0100

    Fix font size of event category in event list.

--- resources/views/partials/events/list.blade.php
+++ resources/views/partials/events/list.blade.php
@@ -49,7 +49,7 @@
                     </div>
                 @endif
             </div>
-            <div class="md:col-span-2 flex items-center px-6 md:px-0 mb-1 md:mb-0">
+            <div class="md:col-span-2 flex items-center px-6 md:px-0 mb-1 md:mb-0 text-sm">
                 {{-- Category --}}
                 <svg class="flex-shrink-0 mr-1.5 h-5 w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M17.707 9.293a1 1 0 010 1.414l-7 7a1 1 0 01-1.414 0l-7-7A.997.997 0 012 10V5a3 3 0 013-3h5c.256 0 .512.098.707.293l7 7zM5 6a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
                 <div>{{$event->category->name}}</div>
