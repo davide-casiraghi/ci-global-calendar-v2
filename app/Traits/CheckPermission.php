9cbda6bfde62a55f59aa0420d178c2ebdd9cff5e
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-02-13 08:30:03 +0100

    Fix user_id property not exist, Scrutinizers errors for the CheckPermission trait.

--- app/Traits/CheckPermission.php
+++ app/Traits/CheckPermission.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Auth;
 
 trait CheckPermission
 {
+    /**
+     * Check if the user has the permission.
+     **/
     public function checkPermission(string $permissionName)
     {
         if (!Auth::user()->hasPermissionTo($permissionName)) {
@@ -14,17 +17,18 @@ trait CheckPermission
     }
 
     /**
-    * Check permission and also allows the owner of the model.
+    * Check the permission and also allows the owner of the model.
     **/
     public function checkPermissionAllowOwner(string $permissionName, $entity)
     {
+        if (!property_exists($entity, 'user_id')) {
+            throw new AccessDeniedException("The user can't be owner of this resource.", 403);
+        }
+
         $userId = $entity->user_id ?? 'none';
-        
         if (!( Auth::user()->hasPermissionTo($permissionName) || Auth::id() === $userId)) {
-            throw new AccessDeniedException("You have not the permission to view this page", 403);
+            throw new AccessDeniedException("You have not the permission to view this page.", 403);
         }
     }
-
-
 
 }
