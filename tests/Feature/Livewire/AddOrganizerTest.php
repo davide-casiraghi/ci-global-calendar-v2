85ae5c023935a0577b4e1c9b3d91a2cdca607b88
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-02-22 11:13:55 +0100

    Add AddOrganizerTest for livewire component.

---
+++ tests/Feature/Livewire/AddOrganizerTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Tests\Feature\Livewire;
+
+use App\Models\Organizer;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
+use Tests\TestCase;
+use App\Http\Livewire\AddOrganizer;
+
+class AddOrganizerTest extends TestCase
+{
+    use WithFaker;
+    use RefreshDatabase; // empty the test DB
+
+    /**
+     * Populate test DB with dummy data.
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // Write to log file
+        file_put_contents(storage_path('logs/laravel.log'), "");
+
+        // Seeders - /database/seeds
+        $this->seed();
+    }
+
+    /** @test */
+    public function itShouldShowTheAddOrganizerModal()
+    {
+        $organizers = [];
+        $selected = [];
+
+        Livewire::test(AddOrganizer::class, [$organizers, $selected])
+            ->assertDontSee('Create new organizer')
+            ->set('showModal', true)
+            ->assertSee('Create new organizer');
+    }
+
+    /** @test */
+    public function itShouldStoreTheNewOrganizerOnDb()
+    {
+        $user = $this->authenticateAsSuperAdmin();
+
+        $organizers = [];
+        $selected = [];
+
+        Livewire::test(AddOrganizer::class, [$organizers, $selected])
+        ->set('newOrganizer.name', 'test name')
+        ->set('newOrganizer.surname', 'test surname')
+        ->set('newOrganizer.email', 'test@email.com')
+        ->set('newOrganizer.description', 'test description')
+        ->set('newOrganizer.website', 'https://www.test.com')
+        ->call('saveOrganizer');
+
+        //dd(Organizer::all());
+
+        $this->assertTrue(Organizer::where('name', 'test name')->exists());
+    }
+
+    /** @test  */
+    public function eventCreatePageShouldContainAddOrganizerComponent()
+    {
+        $user = $this->authenticateAsSuperAdmin();
+
+        $this->get('/events/create')->assertSeeLivewire('add-organizer');
+    }
+
+    /** @test  */
+    public function organizerNameShouldBeRequired()
+    {
+        $user = $this->authenticateAsSuperAdmin();
+
+        $organizers = [];
+        $selected = [];
+        Livewire::test(AddOrganizer::class, [$organizers, $selected])
+            ->set('newOrganizer.name', '')
+            ->set('newOrganizer.surname', 'test surname')
+            ->set('newOrganizer.email', 'test@email.com')
+            ->set('newOrganizer.description', 'test description')
+            ->set('newOrganizer.website', 'https://www.test.com')
+            ->call('saveOrganizer')
+            ->assertHasErrors(['newOrganizer.name' => 'required']);
+    }
+}
