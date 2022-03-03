1dd5c2edb0c6befc860e6a3d3969fcf1e7ac1645
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-03-03 16:26:10 +0100

    Before refactoring hamburgher.

--- resources/views/partials/navigation/navigation.blade.php
+++ resources/views/partials/navigation/navigation.blade.php
@@ -1,4 +1,8 @@
-<div class="relative bg-calendarGold z-40" x-data="Components.popover({ open: false, focus: false })" @keydown.escape="onEscape" @close-popover-group.window="onClosePopoverGroup">
+<div class="relative bg-calendarGold z-40"
+     x-data="Components.popover({ open: false, focus: false })"
+     @keydown.escape="onEscape"
+     @close-popover-group.window="onClosePopoverGroup"
+>
     <div class="max-w-7xl mx-auto px-4 sm:px-6">
         <div class="flex justify-between items-center md:justify-start md:space-x-10 h-14 items-stretch">
             <div class="flex justify-start lg:w-0 lg:flex-1 items-center items-stretch">
@@ -14,7 +18,11 @@
                 @include('partials.navigation.frontend-desktop')
             </div>
             <div class="-mr-2 -my-2 md:hidden">
-                <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" @click="toggle" @mousedown="if (open) $event.preventDefault()" aria-expanded="false" :aria-expanded="open.toString()">
+                <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                        @click="toggle"
+                        @mousedown="if (open) $event.preventDefault()"
+                        aria-expanded="false"
+                        :aria-expanded="open.toString()">
                     <span class="sr-only">Open menu</span>
                     <svg class="h-6 w-6" x-description="Heroicon name: outline/menu" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
