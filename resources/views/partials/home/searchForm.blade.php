8c3ac5b59bf7358d051ce96ac1ed011ea3195e1f
Davide Casiraghi <davide.casiraghi@gmail.com>
2022-03-04 10:49:35 +0100

    Add alpine tooltips in homepage (when, where).

--- resources/views/partials/home/searchForm.blade.php
+++ resources/views/partials/home/searchForm.blade.php
@@ -28,7 +28,12 @@
             </div>
         </div>
         <div class="md:col-span-2 mt-5 md:mt-0">
-            <div class="font-bold text-white">@lang('homepage-search.where')</div>
+            <div class="font-bold text-white">
+                @lang('homepage-search.where')
+                <svg x-data x-tooltip="{{__('homepage-search.where_tooltip')}}" @click="$tooltip('{{__('homepage-search.where_tooltip')}}')"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="flex-shrink-0 mr-1.5 h-4 w-4 inline text-white ml-1">
+                    <path fill="currentColor" d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM256 128c17.67 0 32 14.33 32 32c0 17.67-14.33 32-32 32S224 177.7 224 160C224 142.3 238.3 128 256 128zM296 384h-80C202.8 384 192 373.3 192 360s10.75-24 24-24h16v-64H224c-13.25 0-24-10.75-24-24S210.8 224 224 224h32c13.25 0 24 10.75 24 24v88h16c13.25 0 24 10.75 24 24S309.3 384 296 384z"/>
+                </svg>
+            </div>
             @livewire('continent-country-region', [
                 'selectedContinent' => old('continent_id', $searchParameters['continent_id']),
                 'selectedCountry' => old('country_id', $searchParameters['country_id']),
@@ -59,7 +64,12 @@
 
         </div>
         <div class="md:col-span-2 mt-5 md:mt-0">
-            <div class="font-bold text-white">@lang('homepage-search.when')</div>
+            <div class="font-bold text-white">
+                @lang('homepage-search.when')
+                <svg x-data x-tooltip="{{__('homepage-search.when_tooltip')}}" @click="$tooltip('{{__('homepage-search.when_tooltip')}}')"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="flex-shrink-0 mr-1.5 h-4 w-4 inline text-white ml-1">
+                    <path fill="currentColor" d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM256 128c17.67 0 32 14.33 32 32c0 17.67-14.33 32-32 32S224 177.7 224 160C224 142.3 238.3 128 256 128zM296 384h-80C202.8 384 192 373.3 192 360s10.75-24 24-24h16v-64H224c-13.25 0-24-10.75-24-24S210.8 224 224 224h32c13.25 0 24 10.75 24 24v88h16c13.25 0 24 10.75 24 24S309.3 384 296 384z"/>
+                </svg>
+            </div>
 
             @include('partials.forms.inputFlatPickrDatePicker', [
                             'class' => 'flatpickr date future',
